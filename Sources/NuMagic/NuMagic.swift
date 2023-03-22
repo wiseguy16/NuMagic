e6fb8f597d71fa35b1ce8d83a10556e1366a7394
wiseguy16 <gergusa04@gmail.com>
2023-03-22 16:51:25 -0400

    updating name prop

--- Sources/NuMagic/NuMagic.swift
+++ Sources/NuMagic/NuMagic.swift
@@ -1,11 +1,17 @@
-public struct NuMagic {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+public class NuMagic {
+  public private(set) var text = "Hello, World!"
+  public private(set) var nuName = "Original"
+  
+  public init() {
+  }
   
   public func setMagic(with name: String) {
+    print("Current name is \(nuName)")
+  }
+  
+  public func updateMagic(with name: String) {
     print("Name is now \(name)!!!")
-    
+    self.nuName = name
+    print("Current name is \(nuName)")
   }
 }
